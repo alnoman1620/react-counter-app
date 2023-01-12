03a73b5abc845226d604b5a1a1b5434d3a5aac5f
alnoman1620 <al.noman1620@gmail.com>
2023-01-12 23:58:05 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
 }
 
+function Counter(){
+  const [count, setCount] = useState(0)
+  const increaseHandle = () => setCount(count + 1)
+  const decreaseHandle = () => {
+    if(count > 0){
+      setCount(count - 1)
+    }
+    else{
+      alert('Decreasing smaller than 0 is not possible')
+    }
+  }
+  
+  return(
+    <div className="counter">
+      <h1>Counter : {count}</h1>
+      <button onClick={increaseHandle}>Increase</button>
+      <button onClick={decreaseHandle}>Decrease</button>
+    </div>
+  )
+}
 export default App;
